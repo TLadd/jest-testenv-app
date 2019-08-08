2e3b0ee13b383894a59a726421167d701c0d713a
Thomas Ladd <thomas.ladd@stackpath.com>
2019-08-08 11:34:26 -0500

    Test Case showing that jest-environment-jsdom-fourteen is not being used

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import "@testing-library/react/cleanup-after-each";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, fireEvent } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+// Passes
+it("calls onSubmit when the button is clicked", () => {
+  const onSubmit = jest.fn();
+  const { getByTestId } = render(<App onSubmit={onSubmit} />);
+
+  fireEvent.click(getByTestId("submit-button"));
+
+  expect(onSubmit).toHaveBeenCalled();
+});
+
+// Fails
+it("calls onSubmit when the span inside the button is clicked", () => {
+  const onSubmit = jest.fn();
+  const { getByTestId } = render(<App onSubmit={onSubmit} />);
+
+  fireEvent.click(getByTestId("submit-button-span"));
+
+  expect(onSubmit).toHaveBeenCalled();
 });
